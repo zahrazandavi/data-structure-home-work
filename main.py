dfb844762dd3584322b132a1dca68528ab7440e7
zahrazandavi <zahraa.zaandavi@gmail.com>
2024-05-08 19:00:35 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+class Queue:
+    # max_size: size of Q
+    # Q: Array
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.Q = [0] * max_size
+        self.num = 0
+        self.first = 0
+
+    def enqueue(self, item):
+        if self.num >= self.max_size:
+            raise Exception("Queue overflow")
+
+        self.Q[(self.num + self.first) % self.max_size] = item
+        self.num += 1
+
+    def dequeue(self):
+        if self.num == 0:
+            raise Exception("Queue empty")
+
+        item = self.Q[self.first]
+        self.first = (self.first + 1) % self.max_size
+        self.num -= 1
+        return item
+
+    def front(self):
+        if self.num == 0:
+            raise Exception("Queue empty")
+        return self.Q[self.first]
+
+    def is_empty(self):
+        return self.num == 0
+
+    def size(self):
+        return self.num
+
+    def is_full(self):
+        return self.num >= self.max_size
+
+    def get(self, i):
+        if i < 0 or i >= self.num:
+            raise Exception("Overflow Index")
+        return self.Q[(self.first + i) % self.max_size]
